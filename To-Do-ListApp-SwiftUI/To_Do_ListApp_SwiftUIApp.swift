40de98ecd69d24a2ac5d2981c18132fa620189cd
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-12 01:46:57 +0530

    Initial Commit

---
+++ To-Do-ListApp-SwiftUI/To_Do_ListApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp_SwiftUIApp.swift
+//  To-Do-ListApp-SwiftUI
+//
+//  Created by Nimish Sharad Mothghare on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ To-Do-ListApp-SwiftUITests/To_Do_ListApp_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp_SwiftUITests.swift
+//  To-Do-ListApp-SwiftUITests
+//
+//  Created by Nimish Sharad Mothghare on 12/03/25.
+//
+
+import Testing
+@testable import To_Do_ListApp_SwiftUI
+
+struct To_Do_ListApp_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
