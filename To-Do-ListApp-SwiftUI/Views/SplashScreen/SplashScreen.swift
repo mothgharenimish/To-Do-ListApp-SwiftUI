eb088a0a6dc909f46939b67391d1f2639cb6cc9a
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-14 00:41:06 +0530

    Xcode Update Changes

---
+++ To-Do-ListApp-SwiftUI/Views/SplashScreen/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  To-Do-ListApp-SwiftUI
+//
+//  Created by Nimish Sharad Mothghare on 13/03/25.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
